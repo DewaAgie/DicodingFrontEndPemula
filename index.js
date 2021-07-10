459152160382f76eeeeddadbac2f68566813c888
Dewa Putu Agie Aditya <agixdewa99@gmail.com>
2021-07-10 22:25:37 +0800

    event capturing

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-// membuat custom event
-const changeCaption = new Event("changeCaption");
-
-window.addEventListener('load', function(){
-  const element = document.getElementById('tombol');
-  element.addEventListener('changeCaption', customEventHandler);
-  element.addEventListener('click', function(){
-    element.dispatchEvent(changeCaption);
-  })
-})
-function customEventHandler(ev){
-  console.log(`Event ${ev.type} telah dijalankan`);
-  const caption = document.getElementById('caption');
-  caption.innerText = "Anda telah membangkitkan custom event";
+const divs = document.getElementsByTagName("div");
+for(let el of divs){
+    el.addEventListener("click", function(){
+        alert("ELEMEN "+el.getAttribute("id").toUpperCase());
+    }, true)
 }
