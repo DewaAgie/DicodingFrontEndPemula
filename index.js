b332f7dbf05c84ea852f1ea562675ce0145b0026
Dewa Putu Agie Aditya <agixdewa99@gmail.com>
2021-07-10 22:45:36 +0800

    input event onfocus onblur

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
-const submitForm = document.getElementById('formDataDiri');
-submitForm.addEventListener('submit', function(e){
-  const nama = document.getElementById('nama').value;
-  const domisili = document.getElementById('domisili').value;
-  const message = `Halo ${nama} bagaimana cuacanya di ${domisili}`
+// // event on input
+// document.addEventListener('DOMContentLoaded', function (){
+//     const inputMaxLengthOnLoad = document.getElementById("inputNamaPanggilan").maxLength;
+//     document.getElementById("sisaKarakter").innerText = inputMaxLengthOnLoad;
+// });
 
-  document.getElementById('messageAfterSubmit').innerText = message
-  e.preventDefault();
-})
+// document.getElementById("inputNamaPanggilan").addEventListener("input", function(){
+//     const jumlahKarakterDiketik = document.getElementById("inputNamaPanggilan").value.length;
+//     const jumlahKarakterMaksimal = document.getElementById("inputNamaPanggilan").maxLength;
+//     console.log(jumlahKarakterDiketik, jumlahKarakterMaksimal);
+//     const sisaKarakterUpdate = jumlahKarakterMaksimal - jumlahKarakterDiketik;
+//     document.getElementById("sisaKarakter").innerText = sisaKarakterUpdate;
+//     if (sisaKarakterUpdate == 0 ){
+//         document.getElementById("sisaKarakter").innerText = "batas maksimal tercapai!";
+//     } else if(sisaKarakterUpdate <= 5){
+//         document.getElementById("notifikasiSisaKarakter").style.color = "red";
+//     } else{
+//         document.getElementById("notifikasiSisaKarakter").style.color = "black";
+//     }
+// });
+
+// event on blur
+document.addEventListener('DOMContentLoaded', function (){
+  const inputMaxLengthOnLoad = document.getElementById("inputNamaPanggilan").maxLength;
+  document.getElementById("sisaKarakter").innerText = inputMaxLengthOnLoad;
+  document.getElementById("notifikasiSisaKarakter").style.visibility = "hidden";
+});
+
+document.getElementById("inputNamaPanggilan").addEventListener("input", function(){
+  const jumlahKarakterDiketik = document.getElementById("inputNamaPanggilan").value.length;
+  const jumlahKarakterMaksimal = document.getElementById("inputNamaPanggilan").maxLength;
+  console.log(jumlahKarakterDiketik, jumlahKarakterMaksimal);
+  const sisaKarakterUpdate = jumlahKarakterMaksimal - jumlahKarakterDiketik;
+  document.getElementById("sisaKarakter").innerText = sisaKarakterUpdate;
+  if (sisaKarakterUpdate == 0 ){
+      document.getElementById("sisaKarakter").innerText = "batas maksimal tercapai!";
+  } else if(sisaKarakterUpdate <= 5){
+      document.getElementById("notifikasiSisaKarakter").style.color = "red";
+  } else{
+      document.getElementById("notifikasiSisaKarakter").style.color = "black";
+  }
+});
+
+document.getElementById("inputNamaPanggilan").addEventListener("focus", function(){
+  document.getElementById("notifikasiSisaKarakter").style.visibility = null;
+});
+document.getElementById("inputNamaPanggilan").addEventListener("blur", function(){
+  document.getElementById("notifikasiSisaKarakter").style.visibility = 'hidden';
+});
