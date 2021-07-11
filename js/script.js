f52cb3f33d6f7a1687919da93e6ff506d3ccc215
Dewa Putu Agie Aditya <agixdewa99@gmail.com>
2021-07-11 09:09:22 +0800

    create todo list

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const form = document.getElementById("form");
+
+  form.addEventListener('submit', function(e){
+    e.preventDefault();
+    addTodo();
+  })
+})
