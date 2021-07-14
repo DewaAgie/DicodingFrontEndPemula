9753ccb967519a80d8f006a43c75c2f765a36979
Dewa Putu Agie Aditya <agixdewa99@gmail.com>
2021-07-14 15:09:12 +0800

    mulai web storage

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-  const form = document.getElementById("form");
-
-  form.addEventListener('submit', function(e){
-    e.preventDefault();
-    addTodo();
-  })
-})
